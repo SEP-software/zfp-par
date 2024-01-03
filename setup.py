17b2bdae0b0f5297aa9f6177aa102aeae23288a3
Bob Clapp <clapp@stanford.edu>
2024-01-03 18:08:15 +0000

    zfp setop

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from skbuild import setup
 cmake_args = [
     '-DINSTALL_INCLUDE_FILES=NO'
-    "-DBUILD_ZFPY=YES"
+    "-DBUILD_ZFPY=YES",
+    "-DZFP_WITH_OPENMP=yes"
 ]
 setup(
-        name="zfpy",
-            cmake_args=cmake_args,
+    name="zfpy",
+    cmake_args=cmake_args,
     version="1.0.2",
     author="Peter Lindstrom, Danielle Asher",
     author_email="zfp@llnl.gov",
     url="https://zfp.llnl.gov",
     description="zfp compression in Python",
-    long_description="zfp is a compressed format for representing multidimensional floating-point and integer arrays. zfp provides compressed-array classes that support high throughput read and write random access to individual array elements. zfp also supports serial and parallel compression of whole arrays using both lossless and lossy compression with error tolerances. zfp is primarily written in C and C++ but also includes Python and Fortran bindings.",
+    #long_description="zfp is a compressed format for representing multidimensional floating-point and integer arrays. zfp provides compressed-array classes that support high throughput read and write random access to individual array elements. zfp also supports serial and parallel compression of whole arrays using both lossless and lossy compression with error tolerances. zfp is primarily written in C and C++ but also includes Python and Fortran bindings.",
 )
