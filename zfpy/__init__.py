5db465b06c1e589bc47db9c2df85bf1d3148cac0
Bob Clapp <clapp@stanford.edu>
2024-01-03 20:41:26 +0000

    split python

--- zfpy/__init__.py
+++ zfpy/__init__.py
@@ -1,2 +1,2 @@
-from zfpy_c import (compress_numpy, decompress_numpy)
-from _zfp_par import zfp_p as zfp_parallel
+from .zfpy_c import (compress_numpy, decompress_numpy)
+from ._zfp_par import zfp_p as zfp_parallel
